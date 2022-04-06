d9aac284061c81a750b3d41133593a750e212217
Chatkul Rattanarithikul <billchartkul@hotmail.com>
2022-04-06 21:26:19 +0700

    fix buddaWeb1

--- buddaWeb1/buddaWeb1/Controllers/HomeController.cs
+++ buddaWeb1/buddaWeb1/Controllers/HomeController.cs
@@ -21,12 +21,34 @@ namespace buddaWeb1.Controllers
         public IActionResult Index()
         {
             return View();
+            // UMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+        }
+
+        public IActionResult Content()
+        {
+            return View();
+        }
+
+        public IActionResult Travel()
+        {
+            return View();
+        }
+
+        public IActionResult Viewblog()
+        {
+            return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()

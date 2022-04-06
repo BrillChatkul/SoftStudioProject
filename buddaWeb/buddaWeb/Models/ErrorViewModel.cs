e376f7d6b32723b4a2b436cfaf663e175ce289f8
Chatkul Rattanarithikul <billchartkul@hotmail.com>
2022-04-06 22:46:15 +0700

    Update to WebBudda

--- buddaWeb/buddaWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace buddaWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ webBudda/webBudda/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using webBudda.Models;
+
+namespace webBudda.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+            // UMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+        }
+
+        public IActionResult Content()
+        {
+            return View();
+        }
+
+        public IActionResult Travel()
+        {
+            return View();
+        }
+
+        public IActionResult Viewblog()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

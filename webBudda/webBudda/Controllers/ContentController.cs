b1a379a1c3214a5b80807ce58950520bdfffce15
Apirak Oulis <82326608+Apizz789@users.noreply.github.com>
2022-04-26 17:44:39 +0700

    .

---
+++ webBudda/webBudda/Controllers/ContentController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace buddaWeb.Controllers
+{
+    public class ContentController : Controller
+    {
+
+        private readonly ILogger<ContentController> _logger;
+
+        public ContentController(ILogger<ContentController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Content1()
+        {
+            return View();
+        }
+
+        public IActionResult Content2()
+        {
+            return View();
+        }
+
+        public IActionResult Content2_2()
+        {
+            return View();
+        }
+
+        public IActionResult Content2_3()
+        {
+            return View();
+        }
+
+        public IActionResult Content2_4()
+        {
+            return View();
+        }
+
+        public IActionResult Content3()
+        {
+            return View();
+        }
+
+        public IActionResult Content4()
+        {
+            return View();
+        }
+
+        public IActionResult Content5()
+        {
+            return View();
+        }
+
+        public IActionResult Content6()
+        {
+            return View();
+        }
+
+        public IActionResult Content7()
+        {
+            return View();
+        }
+
+        public IActionResult Content8()
+        {
+            return View();
+        }
+
+        public IActionResult Content9()
+        {
+            return View();
+        }
+
+        public IActionResult Content10()
+        {
+            return View();
+        }
+
+        public IActionResult Content11()
+        {
+            return View();
+        }
+
+        public IActionResult Content12()
+        {
+            return View();
+        }
+
+        public IActionResult Content13()
+        {
+            return View();
+        }
+
+        public IActionResult Content14()
+        {
+            return View();
+        }
+
+        public IActionResult Content15()
+        {
+            return View();
+        }
+
+        public IActionResult Content16()
+        {
+            return View();
+        }
+
+        public IActionResult Content17()
+        {
+            return View();
+        }
+
+        public IActionResult Content18()
+        {
+            return View();
+        }
+
+        public IActionResult Content19()
+        {
+            return View();
+        }
+
+        public IActionResult Content20()
+        {
+            return View();
+        }
+
+    }
+}

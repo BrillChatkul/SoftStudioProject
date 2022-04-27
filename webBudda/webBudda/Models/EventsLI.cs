3c972f0ae00a317c6e7a0825e7eb4b58c40c10e3
Apirak Oulis <82326608+Apizz789@users.noreply.github.com>
2022-04-27 08:27:28 +0700

    up calendar indexadmin contact us

--- webBudda/webBudda/Models/EventsLI.cs
+++ webBudda/webBudda/Models/EventsLI.cs
@@ -6,9 +6,9 @@
 
         public EventsLI()
         {
-            EV = new List<EventsModel>() 
+            EV = new List<EventsModel>()
             {
-         
+
                 new EventsModel {
                                     EventId = 2,
                                     Subject = "วันวิสาขบูชา",
@@ -63,14 +63,364 @@
                                     ThemeColor = "blue",
                                     isFullDay = true
                 },
-   
+                new EventsModel {
+                                    EventId = 8,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "9-Apr-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                 new EventsModel {
+                                    EventId = 9,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "1-Apr-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                 new EventsModel {
+                                    EventId = 10,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "16-Apr-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                 new EventsModel {
+                                    EventId = 11,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "16-Apr-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                   new EventsModel {
+                                    EventId = 12,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "24-Apr-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                   new EventsModel {
+                                    EventId = 13,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "30-Apr-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                   new EventsModel {
+                                    EventId = 14,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "8-May-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                   new EventsModel {
+                                    EventId = 15,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "14-May-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                   new EventsModel {
+                                    EventId = 16,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "23-May-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                   new EventsModel {
+                                    EventId = 17,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "30-May-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                }, new EventsModel {
+                                    EventId = 18,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "7-Jun-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                    new EventsModel {
+                                    EventId = 19,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "14-Jun-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                     new EventsModel {
+                                    EventId = 20,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "22-Jun-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                      new EventsModel {
+                                    EventId = 21,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "28-Jun-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                       new EventsModel {
+                                    EventId = 22,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "6-Jul-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 23,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "12-Jul-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                         new EventsModel {
+                                    EventId = 24,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "21-Jul-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                          new EventsModel {
+                                    EventId = 25,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "28-Jul-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                          new EventsModel {
+                                    EventId = 26,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "5-Aug-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                                 new EventsModel {
+                                    EventId = 27,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "12-Aug-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                                        new EventsModel {
+                                    EventId = 28,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "20-Aug-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                                            new EventsModel {
+                                    EventId = 29,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "26-Aug-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 30,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "3-Sep-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 31,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "10-Sep-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 32,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "18-Sep-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 33,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "25-Sep-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 34,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "3-Oct-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 35,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "10-Oct-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 36,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "18-Oct-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 37,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "24-Oct-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 38,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "1-Nov-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 39,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "8-Nov-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 40,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "16-Nov-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                        new EventsModel {
+                                    EventId = 41,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "23-Nov-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                new EventsModel {
+                                    EventId = 42,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "1-Dec-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                new EventsModel {
+                                    EventId = 43,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "8-Dec-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                new EventsModel {
+                                    EventId = 44,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "16-Dec-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                new EventsModel {
+                                    EventId = 45,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "22-Dec-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+                new EventsModel {
+                                    EventId = 46,
+                                    Subject = "วันพระ",
+                                    Description = "..........",
+                                    Start = "30-Dec-2022",
+                                    End = "NULL",
+                                    ThemeColor = "orange",
+                                    isFullDay = true
+                },
+
             };
-            
+
         }
         public List<EventsModel> GetEvent()
-    {
+        {
             return EV.ToList();
-    }
+        }
 
     }
 
